a639d24e4080937e065980eccafbc2160ae064c3
L9029 <angeloca03@gmail.com>
2025-08-04 11:18:05 -0400

    Se procedio a agregar estilos adicionales al listado de los repositorios asociados al usuario, así como agregar elementos faltantes en el listado.

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -7,37 +7,52 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <p class="text-right mb-4">
+                <a href="{{ route('repositories.create') }}" class="bg-blue-500 text-white font-bold py-2 px-4 rounded text-sm hover:bg-blue-600">
+                    Crear Repositorio
+                </a>
+            </p>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
                 <table>
                     <thead>
                         <tr>
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Descripción</th>
-                            <th>URL</th>
-                            <th>Fecha de Creación</th>
-                            <th>Fecha de Actualización</th>
+                            <th class="px-4 py-2">ID</th>
+                            <th class="px-4 py-2">URL</th>
+                            <th class="px-4 py-2">Descripción</th>
+                            <th class="px-4 py-2">Fecha de Creación</th>
+                            <th class="px-4 py-2">Fecha de Actualización</th>
+                            <th>&nbsp;</th>
+                            <th>&nbsp;</th>
                             <th>&nbsp;</th>
                         </tr>
                     </thead>
                     <tbody>
                         @forelse($repositories as $repository)
-                            <tr>
-                                <td>{{ $repository->id }}</td>
-                                <td>{{ $repository->name }}</td>
-                                <td>{{ $repository->url }}</td>
-                                <td>{{ $repository->description }}</td>
-                                <td>{{ $repository->created_at }}</td>
-                                <td>{{ $repository->updated_at }}</td>
-                                <td>
-                                    <a href="{{ route('repositories.edit', $repository->id) }}" class="text-blue-500 hover:underline">
+                            <tr class="text-center {{ $loop->even ? 'bg-gray-100 hover:bg-gray-200' : 'bg-gray-200 hover:bg-gray-300' }}">
+                                <td class="px-4 py-2">{{ $repository->id }}</td>
+                                <td class="px-4 py-2">{{ $repository->url }}</td>
+                                <td class="px-4 py-2">{{ $repository->description }}</td>
+                                <td class="px-4 py-2">{{ $repository->created_at->format('d-m-Y') }}</td>
+                                <td class="px-4 py-2">{{ $repository->updated_at->format('d-m-Y') }}</td>
+                                <td class="px-4 py-2">
+                                    <a href="{{ route('repositories.show', $repository->id) }}" class="px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">
+                                        Ver
+                                    </a>
+                                </td>
+                                <td class="px-4 py-2">
+                                    <a href="{{ route('repositories.edit', $repository->id) }}" class="px-4 rounded-md bg-blue-500 text-white hover:bg-blue-600">
                                         Editar
                                     </a>
                                 </td>
-                                <td>
-                                    <a href="{{ route('repositories.show', $repository->id) }}" class="text-blue-500 hover:underline">
-                                        Ver
-                                    </a>
+                                <td class="px-4 py-2">
+                                    <form action="{{ route('repositories.destroy', $repository->id) }}" method="POST" onsubmit="return confirm('¿Estás seguro de que deseas eliminar este repositorio?');">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button type="submit" class="px-4 rounded-md bg-red-500 text-white hover:bg-red-600">
+                                            Eliminar
+                                        </button>
+                                    </form>
                                 </td>
                             </tr>
                         @empty
